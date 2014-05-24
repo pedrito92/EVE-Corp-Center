54ff29e4129156b57907df69b1b4413251e7a15e
Florian Neveu <hello@fnev.eu>
2014-05-24 02:25:43 +0200

    Préparation schemas SQL d'installation

---
+++ schemas/schemas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `ecc_pages` (
+  `ID_page` int(11) NOT NULL auto_increment,
+  `name_page` varchar(200) NOT NULL,
+  `slug_page` varchar(200) NOT NULL,
+  PRIMARY KEY  (`ID_page`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+‡
