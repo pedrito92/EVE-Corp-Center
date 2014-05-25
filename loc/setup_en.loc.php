871f40d4bcef9e2385df6e19c9f9dacc46c0df78
Florian Neveu <hello@fnev.eu>
2014-05-25 16:28:43 +0200

    Support des langues

---
+++ loc/setup_en.loc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florianneveu
+ * Date: 25/05/2014
+ * Time: 11:45
+ */
+
+// SETUP
+
+define("WELCOME_SETUP", "Bienvenue sur l'installateur de EVE Corp Center");
+define("SELECT_LANGUAGE", "Sélectionnez une langue :");
+define("NEXT_STEP", "Next");
+
+define("MYSQL_CONNEXION", "Please specify the connection information to your database.");
+define("MYSQL_USER", "User");
+define("MYSQL_PASS", "Password");
+define("MYSQL_HOST", "Host");
+define("MYSQL_DBNAME", "Database");
---
+++ loc/setup_fr.loc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florianneveu
+ * Date: 25/05/2014
+ * Time: 11:45
+ */
+
+// SETUP
+
+define("WELCOME_SETUP", "Bienvenue sur l'installateur de EVE Corp Center");
+define("SELECT_LANGUAGE", "Sélectionnez une langue :");
+define("NEXT_STEP", "Étape suivante");
+
+define("MYSQL_CONNEXION", "Veuillez spécifier les informations de connexion à votre base de données.");
+define("MYSQL_USER", "Utilisateur");
+define("MYSQL_PASS", "Mot de passe");
+define("MYSQL_HOST", "Serveur");
+define("MYSQL_DBNAME", "Base de données");
