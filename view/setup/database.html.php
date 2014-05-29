d946603e798afacfa5d07ba082b760298a2ac2bb
Florian Neveu <hello@fnev.eu>
2014-05-30 00:12:03 +0200

    Correction des liens "Previous step"

--- view/setup/database.html.php
+++ view/setup/database.html.php
@@ -42,9 +42,9 @@
         <input type="text" id="dbNom" name="dbNom" value="<?php echo $form['nomBase']; ?>">
         <br>
 
-        <a href="/?domain=setup"><?php echo PREVIOUS_STEP; ?></a>
+        <a href="<?php echo $_SERVER['PHP_SELF']; ?>?domain=setup"><?php echo PREVIOUS_STEP; ?></a>
         <?php if(isset($erreurPDO) && $erreurPDO == 'ok') { ?>
-            <a href="/?domain=setup&action=setupDatabase"><?php echo NEXT_STEP; ?></a>
+            <a href="<?php echo $_SERVER['PHP_SELF']; ?>?domain=setup&action=setupDatabase"><?php echo NEXT_STEP; ?></a>
         <?php } else { ?>
             <input type="submit" value="<?php echo NEXT_STEP; ?>">
         <?php } ?>
