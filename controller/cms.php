fd325f1bb83fe419e329ff7e4d4313371f3ca356
Florian Neveu <hello@fnev.eu>
2014-06-09 23:36:40 +0200

    Exemple de requete avec Pheal au sein du CMS

--- controller/cms.php
+++ controller/cms.php
@@ -7,6 +7,7 @@
  */
 
 use Pheal\Pheal;
+use Pheal\Core\Config;
 
 class cms {
 
@@ -30,5 +31,35 @@ class cms {
             $response->serverOpen ? "open" : "closed",
             $response->onlinePlayers
         );
+
+
+        echo '<br><br><br><br>';
+
+        $keyID = 2815094;
+        $vCode = "BLHfaEJ6oFt07utSvBfNZG6EpPMHW7VTu3s9CqPZ8JxV9UKR5d0mEnzths7Vhbwp";
+        $characterID = 90719345;
+
+        Config::getInstance()->cache = new \Pheal\Cache\FileStorage('var/phealcache/');
+        Config::getInstance()->access = new \Pheal\Access\StaticCheck();
+
+        $pheal2 = new Pheal($keyID, $vCode, "char");
+
+        try{
+            $response = $pheal2->CharacterSheet(array("characterID" => $characterID));
+
+            echo sprintf(
+                "Hello Visitor, Character %s was created at %s is of the %s race and belongs to the corporation %s",
+                $response->name,
+                $response->DoB,
+                $response->race,
+                $response->corporationName
+            );
+        } catch (\Pheal\Exceptions\PhealException $e) {
+            echo sprintf(
+                "an exception was caught! Type: %s Message: %s",
+                get_class($e),
+                $e->getMessage()
+            );
+        }
     }
 } 
