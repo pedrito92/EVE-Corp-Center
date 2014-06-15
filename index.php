c5f4ba0f1e8daee54896f4a1531bcdfac2afb24e
Florian Neveu <hello@fnev.eu>
2014-06-16 00:19:31 +0200

    Tests configuration serveur

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
  * Time: 22:46
  */
 
+if ( version_compare( PHP_VERSION, '5.2' ) < 0 )
+{
+    print( "<h1>Unsupported PHP version " . PHP_VERSION . "</h1>" );
+    print( "<p>EVE Corp Center does not run with PHP version lower than 5.2.</p>".
+        "<p>For more information about supported software please visit ".
+        "<a href=\"http://www.evecorpcenter.com\" >EVE Corp Center Website</a></p>" );
+    exit;
+}
+
+if ( !ini_get( "date.timezone" ) )
+{
+    date_default_timezone_set( "UTC" );
+}
+
 require_once('autoload.php');
 session_start();
+
 $app = new kernel\RoutingHandler();
