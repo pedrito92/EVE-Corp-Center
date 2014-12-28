6da0b636e7f36b50e385e22a9cfef74da35c47bd
Florian Neveu <hello@fnev.eu>
2014-12-29 00:49:37 +0100

    Clean du code

--- autoload.php
+++ autoload.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: florianneveu
- * Date: 10/06/2014
- * Time: 21:45
- */
 
 function autoloader($class) {
-
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     if(file_exists($path.".php")){
@@ -15,7 +8,6 @@ function autoloader($class) {
     } elseif(file_exists('lib/'.$path.'.php')){
         require_once('lib/'.$path.'.php');
     }
-
 }
 
 spl_autoload_register('autoloader');
--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: florianneveu
- * Date: 22/05/2014
- * Time: 22:46
- */
-
-if ( version_compare( PHP_VERSION, '5.2' ) < 0 )
-{
-    print( "<h1>Unsupported PHP version " . PHP_VERSION . "</h1>" );
-    print( "<p>EVE Corp Center does not run with PHP version lower than 5.2.</p>".
+if (version_compare(PHP_VERSION, '5.2') < 0){
+    print("<h1>Unsupported PHP version " . PHP_VERSION . "</h1>");
+    print("<p>EVE Corp Center does not run with PHP version lower than 5.2.</p>".
         "<p>For more information about supported software please visit ".
-        "<a href=\"http://www.evecorpcenter.com\" >EVE Corp Center Website</a></p>" );
+        "<a href=\"http://www.evecorpcenter.com\" >EVE Corp Center Website</a></p>");
     exit;
 }
 
-if ( !ini_get( "date.timezone" ) )
-{
-    date_default_timezone_set( "UTC" );
+if(!ini_get("date.timezone")) {
+    date_default_timezone_set("UTC");
 }
+ini_set( "display_errors", 0);
 
 require_once('autoload.php');
 session_start();
