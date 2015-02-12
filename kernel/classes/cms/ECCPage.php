f806bc097fb913b4d398338df862a8cb00661310
Florian Neveu <hello@fnev.eu>
2015-02-12 15:25:16 +0100

    Suppression des libs pour une gestion via composer

---
+++ kernel/classes/cms/ECCPage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florianneveu
+ * Date: 04/02/15
+ * Time: 09:21
+ */
+
+namespace kernel\classes\cms;
+
+
+class ECCPage {
+
+}
---
+++ kernel/classes/game/ECCApiKey.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florianneveu
+ * Date: 08/02/15
+ * Time: 02:03
+ */
+
+namespace kernel\classes\game;
+
+
+class ECCApiKey {
+
+}
