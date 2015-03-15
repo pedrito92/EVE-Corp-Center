88fb667fbc06c43d9c9fd29e230595c8be57504d
Florian Neveu <hello@fnev.eu>
2015-03-15 11:57:21 +0100

    Suppressions schemas.sql inutile

--- schemas/schemas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ecc_pages` (
-  `ID_page` int(11) NOT NULL auto_increment,
-  `name_page` varchar(200) NOT NULL,
-  `slug_page` varchar(200) NOT NULL,
-  PRIMARY KEY  (`ID_page`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-‡
