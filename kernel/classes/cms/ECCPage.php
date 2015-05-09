8558dd64f0edc5265023416ae0c1c07ecf46aab9
Florian Neveu <hello@fnev.eu>
2015-05-09 17:47:34 +0200

    Modification d'ECCPage et tests de Twig

--- kernel/classes/cms/ECCPage.php
+++ kernel/classes/cms/ECCPage.php
@@ -5,6 +5,7 @@ use kernel\classes\ECCDB;
 use kernel\classes\ECCObject;
 use kernel\classes\ECCINI;
 use kernel\classes\ECCSystem;
+use kernel\classes\ECCTemplate;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 use Twig_Extension_Debug;
@@ -13,30 +14,14 @@ use Twig_Extension_Optimizer;
 class ECCPage extends ECCObject {
 
 	function exec(){
-		$ini		= ECCINI::instance();
-		$cache		= $ini->getVariable('infos', 'cache');
-		$twigParams = array();
+		$ECCTemplate = ECCTemplate::instance();
 
-		if(ECCSystem::getDebug())
-			$twigParams['debug'] = true;
-		if($cache)
-			$twigParams['cache'] = 'var/cache/twig';
-
-
-		$loader	= new Twig_Loader_Filesystem('design/default');
-		$twig	= new Twig_Environment($loader, $twigParams);
-
-		if(ECCSystem::getDebug())
-			$twig->addExtension(new Twig_Extension_Debug());
-
-		$twig->addExtension(new Twig_Extension_Optimizer());
-
-		echo $twig->render($this->attributes['name'].'.html.twig',
-							array('object'		=> (array)$this->attributes,
-								'object_id'		=> $this->ID,
-								'data'			=> $this->data,
-								'current_user'	=> (array)$_SESSION['currentUser']
-							)
+		$ECCTemplate->display(strtolower('pages/'.$this->attributes['name']).'.html.twig',
+			array('object'		=> (array)$this->attributes,
+				'object_id'		=> $this->ID,
+				'data'			=> $this->data,
+				'current_user'	=> (array)$_SESSION['currentUser']
+			)
 		);
 	}
 
